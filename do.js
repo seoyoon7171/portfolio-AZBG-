4dddaa0acbced0a8b36d753bb62e8d377238d1ce
seoyoon7171 <71548172+seoyoon7171@users.noreply.github.com>
2020-11-17 13:58:47 +0900

    final

---
+++ do.js
@@ -0,0 +1,46 @@
+a1 = document.getElementById('a1')
+a2 = document.getElementById('a2')
+b1 = document.getElementById('b1')
+b2 = document.getElementById('b2')
+c1 = document.getElementById('c1')
+c2 = document.getElementById('c2')
+c3 = document.getElementById('c3')
+c4 = document.getElementById('c4')
+c5 = document.getElementById('c5')
+c6 = document.getElementById('c6')
+c7 = document.getElementById('c7')
+
+
+if (a1) {
+    addEventListener(click, )
+}
+if (a2) {
+    addEventListener(click, )
+}
+if (b1) {
+    addEventListener(click, )
+}
+if (b2) {
+    addEventListener(click, )
+}
+if (c1) {
+    addEventListener(click, )
+}
+if (c2) {
+    addEventListener(click, )
+}
+if (c3) {
+    addEventListener(click, )
+}
+if (c4) {
+    addEventListener(click, )
+}
+if (c5) {
+    addEventListener(click, )
+}
+if (c6) {
+    addEventListener(click, )
+}
+if (c7) {
+    addEventListener(click, )
+}
